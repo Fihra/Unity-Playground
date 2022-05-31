7593fe770debf3ff6bed20c9a9ceb82d95a020ca
fihra <fabrofabian@gmail.com>
2022-05-31 08:15:08 -1000

    added end trigger

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,13 +7,22 @@ public class GameManager : MonoBehaviour
 {
     bool gameHasEnded = false;
     public float restartDelay = 1f;
+
+    public GameObject completeLevelUI;
+    
+    public void CompleteLevel()
+    {
+        completeLevelUI.SetActive(true);
+        Debug.Log("LEVEL WON!");
+    }
+
     public void EndGame()
     {
         if(gameHasEnded == false)
         {
             gameHasEnded = true;
             Debug.Log("GAME OVER");
-            Invoke("Restart", restartDelay);
+            Invoke("Restart", restartDelay); 
         }      
     }
 
